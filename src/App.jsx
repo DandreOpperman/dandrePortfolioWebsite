507b3407648abb1cb6406669f209ad3cb7decee7
TheStringKing <dandreopperman@gmail.com>
2024-11-19 17:29:36 -0500

    trouble shoot errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+
 import Testimonials from "./components/Testimonials";
+import Skills from "./components/skills";
 
 function App() {
   return (
